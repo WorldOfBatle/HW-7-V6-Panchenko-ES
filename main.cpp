0266a38d0b94d5edef2a50103a7408bd152ed69c
Evgeny Panchenko <worldbatle@mail.ru>
2025-04-21 19:37:41 +0300

    Создана заготовка под две задачи

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void task1() {
+    std::cout << "--- Task 1 ---\n";
+}
+
+void task2() {
+    std::cout << "--- Task 2 ---\n";
+}
+
+int main() {
+    task1();
+    task2();
+    return 0;
+}
